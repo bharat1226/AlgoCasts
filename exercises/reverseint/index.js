4b0fd6f0ef6aba30d0fa048b0a7db103fc59a16f
Bharat Goli <bharatgoli92@gmail.com>
2020-04-11 19:46:35 -0400

    3. reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt1(n) {
+	// My solution
+	const sign = Math.sign(n);
+
+	return sign * parseInt((n * sign).toString().split('').reverse().join(''));
+}
+
+function reverseInt(n) {
+	// Current Solution
+
+	// parseInt('23-') = '23'
+	// Above trick for parseInt
+	return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''));
+}
 
 module.exports = reverseInt;
