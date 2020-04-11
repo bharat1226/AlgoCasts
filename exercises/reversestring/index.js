7381792d21ee06159921b63cca03802e9aad72af
Bharat Goli <bharatgoli92@gmail.com>
2020-04-11 19:13:51 -0400

    1. Reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse1(str) {
+	// using reverse() built-in array function
+	return str.split('').reverse().join('');
+}
+
+function reverse2(str) {
+	// By using reduce function
+	return str.split('').reduce((prev, curr) => curr + prev, '');
+	// return str.split('').reduce((reversed, character) => character + reversed, '');
+}
+
+function reverse(str) {
+	// using for loop
+	let reversed = '';
+
+	for (let char of str) {
+		reversed = char + reversed;
+	}
+
+	return reversed;
+}
 
 module.exports = reverse;
