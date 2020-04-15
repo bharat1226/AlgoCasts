e5216f9a3bebc72fe84b2e5ba8180bf2e5d06051
Bharat Goli <bharatgoli92@gmail.com>
2020-04-14 21:11:58 -0400

    7. Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,60 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// My Solution
+function sortAndFilter(str) {
+	return str
+		.toLowerCase()
+		.split('')
+		.filter((x) => x !== ' ' || x !== '!' || x !== ',')
+		.sort()
+		.join('');
+}
+
+function anagramsMySolution(stringA, stringB) {
+	return sortAndFilter(stringA) === sortAndFilter(stringB);
+}
+
+// -----------------------------------------------------------
+// Solution 1
+
+function buildCharMap(str) {
+	const charMap = {};
+
+	// use only alphanumerice ---> use regex and replace method
+	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+
+	return charMap;
+}
+
+function anagrams1(stringA, stringB) {
+	const aCharMap = buildCharMap(stringA);
+	const bCharMap = buildCharMap(stringB);
+
+	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+		return false;
+	}
+
+	for (let char in aCharMap) {
+		if (aCharMap[char] !== bCharMap[char]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+// ---------------------------------------------------------------
+// solution 2 ----> might lead to performance concerns --> straight forward
+
+function cleanString(str) {
+	return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+function anagrams(stringA, stringB) {
+	return cleanString(stringA) === cleanString(stringB);
+}
 
 module.exports = anagrams;
