4d776a82dbed4d7d7d604ca3b5836df06b24b629
Bharat Goli <bharatgoli92@gmail.com>
2020-04-15 21:32:37 -0400

    11. Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,55 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+const vowelsArray = ['a', 'e', 'i', 'o', 'u'];
+
+function _isVowel(char) {
+	return vowelsArray.includes(char);
+}
+
+function vowelsMySolution(str) {
+	return str
+		.toLowerCase()
+		.split('')
+		.filter((char) => !!_isVowel(char)).length;
+}
+
+// ------------------------------------------------------------------
+// Solution 1
+
+function vowels1(str) {
+	let count = 0;
+
+	// If we use an array like above, it will become easier for future engineer
+	const checker = 'aeiou';
+
+	for (let char of str.toLowerCase()) {
+		// Trick here is using includes
+		if (checker.includes(char)) {
+			count++;
+		}
+	}
+
+	return count;
+}
+
+// ------------------------------------------------------------------
+// Solution 2 ---> slightly more condensed version
+
+function vowels(str) {
+	// Using regex is so easy --> its like cheating :p
+
+	// we add 'g' and 'i' options at end of regex -->
+	// 'g' => do not stop at first match
+	// 'i' => case 'insensivtive'
+	const matches = str.match(/[aeiou]/gi);
+
+	// str.match will return an array of all the matches found
+	// if no matches are found, match function will return null
+
+	return matches ? matches.length : 0;
+}
+
+// iterable solution is safe and best ---> my solution
 
 module.exports = vowels;
