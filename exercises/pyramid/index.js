9d02cc355e77285ec2ae77b7d2551050c7436b9a
Bharat Goli <bharatgoli92@gmail.com>
2020-04-15 21:15:17 -0400

    10. Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,48 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+//  Solution #1 ---> legacy
+function pyramid1(n) {
+	const midPoint = Math.floor((2 * n - 1) / 2);
+	for (let row = 0; row < n; row++) {
+		let level = '';
+
+		for (let column = 0; column < 2 * n - 1; column++) {
+			if (midPoint - row <= column && midPoint + row >= column) {
+				level += '#';
+			} else {
+				level += ' ';
+			}
+		}
+
+		console.log(level);
+	}
+}
+
+function pyramid(n, row = 0, level = '') {
+	const midPoint = Math.floor((2 * n - 1) / 2);
+
+	// Base case
+	if (row === n) {
+		return;
+	}
+
+	// If string length is 2n-1 ==> console.log and move on to next row
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		return pyramid(n, row + 1);
+	}
+
+	// Logic for adding strings
+
+	let add;
+	if (midPoint - row <= level.length && midPoint + row >= level.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	}
+
+	pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
